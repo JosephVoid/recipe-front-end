32d32f6b78eaa8f2a4a5da181ccd46a91c95b6c0
Yoseph <yosephten@gmail.com>
2024-03-28 11:00:12 +0300

    `feat`: routing done

---
+++ src/pages/create-recipes.tsx
@@ -0,0 +1,3 @@
+export default function CreateRecipes() {
+  return <div className="">Create Recipes</div>;
+}
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,9 +1,26 @@
 import NavBar from "../components/nav-bar";
+import { Link, Route, Switch } from "wouter";
+import Recipes from "./recipes";
+import CreateRecipes from "./create-recipes";
+import Shopping from "./shopping";
+import EditRecipes from "./edit-recipes";
+import SignUp from "./signup";
 
 export default function Dashboard() {
   return (
     <div className="w-full">
       <NavBar />
+      <Route path="/" component={Recipes} />
+      <Switch>
+        <Route path="/recipes" nest>
+          <Route path="/" component={Recipes} />
+          <Route path="/create-recipes" component={CreateRecipes} />
+          <Route path="/edit-recipes" component={EditRecipes} />
+        </Route>
+        <Route path="/shopping" component={Shopping} />
+        <Route path="/signup" component={SignUp} />
+        <Route>404: No such page</Route>
+      </Switch>
     </div>
   );
 }
